ab5bc4a52da65a02b90be3d9cc2f2f001697263e
Caelmab <barrylamb530@gmail.com>
2023-02-23 16:00:20 -0600

    09 commit

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,15 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   // eslint-disable-next-line array-callback-return
+   const cards = characters.map((character) => {
+      <Card
+         key={character.id}
+         name={character.name} 
+         species={character.species} 
+         gender={character.gender} 
+         image={character.image} 
+      />
+   })
+   return <div>{cards}</div>;
 }
--- 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
@@ -1,8 +1,12 @@
 export default function SearchBar(props) {
+   const {onSearch} = props
+   const handleclick = () => {
+      onSearch("cualquiera");
+   };
    return (
       <div>
-         {/* <input type='search' />
-      <button onClick={}>Agregar</button> */}
+          <input type='search' />
+      <button onClick={handleclick}>Agregar</button> 
       </div>
    );
 }
